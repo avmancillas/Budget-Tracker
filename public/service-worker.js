0c50deb50b02783c667e822eba9fa7b8fdfd9fd1
Avi Mancillas <avmancillas@gmail.com>
2021-07-15 23:09:53 -0500

    added atlas connect

--- public/service-worker.js
+++ public/service-worker.js
@@ -57,11 +57,5 @@ self.addEventListener("fetch", evt =>{
         );
         return;
     }
-    evt.respondWith(
-        caches.open(CACHE_NAME.then(cache => {
-            return cache.match(evt.request).then(response =>{
-                return response || fetch(evt.request);
-            });
-        })
-    ));
+   
 });
